7f65afc1a0e8d094865f8842eb0d592f0d62eeeb
gavwan01 <gavin.qd.wang@gmail.com>
2018-12-09 09:15:54 +0800

    improved code

--- app/src/main/java/com/example/gavwan01/myapplication/MainActivity.java
+++ app/src/main/java/com/example/gavwan01/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gavwan01.myapplication;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +15,11 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv_hello = (TextView) findViewById(R.id.tv_hello);
         tv_hello.setText("中国你好");
-        tv_hello.setTextColor(Color.RED);
+        tv_hello.setTextColor(0x67890548);
         tv_hello.setTextSize(40);
+        tv_hello.setBackgroundColor(Color.YELLOW);
+
+
 
     }
 }
